60f47b96b41b02798c508b945d4d1c5029465def
MendesMaquiansPCP <rafael.gois@mendesmaquinas.com.br>
2022-10-05 09:46:20 -0300

    implementacao da listagem do recyclerview

--- app/src/main/java/com/example/todomysqlcurso/adapter/TarefaAdapter.java
+++ app/src/main/java/com/example/todomysqlcurso/adapter/TarefaAdapter.java
@@ -1,40 +1,54 @@
 package com.example.todomysqlcurso.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.todomysqlcurso.R;
 import com.example.todomysqlcurso.model.Tarefa;
 
 import java.util.List;
 
 public class TarefaAdapter extends RecyclerView.Adapter<TarefaAdapter.MyViewHolder>  {
 
+    List<Tarefa> tarefas;
+
     public TarefaAdapter(List<Tarefa> tarefas) {
+        this.tarefas = tarefas;
     }
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        View itemLista = LayoutInflater.from(parent.getContext()).inflate(R.layout.lista_tarefa_adapter,parent,false);
+        return new MyViewHolder(itemLista);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
 
+        Tarefa tarefa = tarefas.get(position);
+        holder.nomeTarefa.setText(tarefa.getNomeTarefa());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return tarefas.size();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
 
+        TextView nomeTarefa;
+
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            nomeTarefa = itemView.findViewById(R.id.textTarefa);
         }
     }
 
